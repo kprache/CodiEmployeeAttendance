52c28db2ed569393a577b22a794b341066c9de3f
kprache <43300630+kprache@users.noreply.github.com>
2018-10-07 00:41:10 +0530

    added db connction and along with the css, js

---
+++ EmployeeAttendanceReport/WebContent/FirstFile.js
@@ -0,0 +1,14 @@
+
+$("#email").focus(function(){
+    $("#emailLabel").addClass("show");
+    $(this).val('')
+}).blur(function(){
+    $("#emailLabel").removeClass("show");
+});
+
+$("#password").focus(function(){
+    $("#passwordLabel").addClass("show");
+    $(this).val('')
+}).blur(function(){
+    $("#passwordLabel").removeClass("show");
+});
